ecd343a72056da7c8c1561b21dc295d2c8ba8d02
harshi-cse <Harsa.throwaway@gmail.com>
2022-02-02 21:09:44 -0800

    starter code and failing test

--- SkillDemoOne.java
+++ SkillDemoOne.java
@@ -1,1 +1,8 @@
-public class SkillDemoOne {}
+public class SkillDemoOne {
+    public static boolean isFactorOf(int x, int y) {
+        if ( y != 0 && x%y == 0) {
+            return true;
+        }
+        return false;
+    }
+}
